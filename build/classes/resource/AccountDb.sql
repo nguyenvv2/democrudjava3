028c9b7f60704dfba83eb77869bdcfb091dba2ef
nguyenvv2 <nguyenvv09@gmail.com>
2022-07-21 14:54:28 +0700

    update

--- build/classes/resource/AccountDb.sql
+++ build/classes/resource/AccountDb.sql
@@ -1,9 +1,9 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
- */
-/**
- * Author:  nguyenvv
- * Created: Jul 21, 2022
- */
 
+CREATE table account(
+    id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    tai_khoan VARCHAR(50),
+    mat_khau VARCHAR(50)
+)
+
+Tạo class sinh vien : ma_sinh_vien int,
+ ten_sinh_vien varchar, que_quan varchar
